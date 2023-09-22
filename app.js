0ce2b264c01de065becc6ee12558dbba68b3a951
unknown <ranatanveerhassan04@gmail.com>
2023-09-22 18:45:20 +0500

    aabb

--- app.js
+++ app.js
@@ -3,4 +3,9 @@ alert("welcome to JS Land...\Happy Coding!")
 alert("Welcome to JS Land..")
 alert("Happy Coding!\  prevent this page from creating addintion dailogs")
 alert("Hello...I can run JS through my web browse`s console")
-alert("Rana Tanveer Hassan Cco 116850 ,Sylani Mass IT trening\Thank you for your visit my website")
+
+var line1 ="Rana Tanveer Hassan Cco 116850 ,Sylani Mass IT trening--";
+var line2 ="Thank you for visit my website";
+var message=line1+"\n"+line2;
+alert(message);
+
