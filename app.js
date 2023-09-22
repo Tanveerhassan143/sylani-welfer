86063945849b6178b9b40ed8f4943dcb5d2f1427
unknown <ranatanveerhassan04@gmail.com>
2023-09-22 19:12:49 +0500

    aaaaa

--- app.js
+++ app.js
@@ -11,4 +11,8 @@ var line4 ="PI";
 var line5 ="P";
 var message=line1+"\n"+line2+"\n"+line3+"\n"+line4+"\n"+line5;
 alert(message);
-
+alert("my email address is ranatanveerhassan04@gmail.com")
+var line1 ="i am trying to le learm from the Book A smarter";
+var line2 ="way to learn JavaScript";
+var message=line1+"\n"+line2;
+alert(message);
